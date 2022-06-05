3e98c32592b2ae72ff5e52013f373e14d18fae27
developerashikur1 <masik4506@gmail.com>
2022-06-05 19:28:40 +0600

    new code pushed

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,21 +1,23 @@
 import { Box, Fade, Grid, Tooltip, Typography } from '@mui/material';
 import React from 'react';
+import Bg1 from '../../../static-images/home-bg-1.jpg';
 import Logo from '../../../static-images/logo.png';
 import LiveSearch from './LiveSearch';
 
 const Header = () =>{
     return(
         <>
-            <Box>
+            <Box style={{background:`url(${Bg1})`}}>
+
                 {/* responsive for Desktop mode only */}
                 <Grid container spacing={0}>
-
+                
                 {/* Logo grid */}
                     <Grid item xs={3} sm={1.5}>
                         <Tooltip title="Gym Xtore" arrow TransitionComponent={Fade} TransitionProps={{timeout:800}}>
                             <Box sx={{display:'flex', flexDirection:'column', alignItems:'center', justifyItems:'center'}}>
                                 <img style={{width:'62px'}} src={Logo} alt=''/>
-                                <Typography sx={{color:'green'}} gutterBottom component="div" variant="h6" fontSize={16} fontWeight={400}>
+                                <Typography sx={{color:'green'}} gutterBottom component="div" variant="h6" fontSize={14} fontWeight={400}>
                                     Gym Xtore
                                 </Typography>
                             </Box>
--- src/components/Shared/Header/LiveSearch.js
+++ src/components/Shared/Header/LiveSearch.js
@@ -1,14 +1,31 @@
+import { SearchOutlined } from '@mui/icons-material';
 import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
-import { IconButton, Input } from '@mui/material';
+import { Avatar, Box, IconButton, InputBase, Typography } from '@mui/material';
+import { deepPurple } from '@mui/material/colors';
 import React from 'react';
 
 const LiveSearch = () =>{
     return(
         <>
-            <Input placeholder="Hello world"/>
-            <IconButton aria-label="delete" size="large">
-                <LocalMallOutlinedIcon fontSize="inherit" />
-            </IconButton>
+            <Box sx={{display:'flex', alignItems:'center'}}>
+                <Box sx={{display:'flex', alignItems:'center'}}>
+                    <InputBase
+                        sx={{borderBottom:'1px solid lightGreen', color:'white', width:'100%',mr:-4}} 
+                        placeholder="Hello world"
+                        color="success"
+                    />
+                    <SearchOutlined sx={{ color:'lightgreen'}} />
+                </Box>
+                <Box sx={{display:'flex', alignItems:'center', ml:3}}>
+                    <Avatar sx={{ bgcolor: deepPurple[500] , width:30, height:30}}>A</Avatar>
+                    <Typography sx={{color:'green'}} gutterBottom component="div" variant="h6" fontSize={14} fontWeight={400}>
+                        Log In
+                    </Typography>
+                </Box>
+                <IconButton aria-label="delete" size="large">
+                    <LocalMallOutlinedIcon fontSize="inherit" />
+                </IconButton>
+            </Box>
         </>
     )
 };
